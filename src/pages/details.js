86a1174d4b6785bdba09a6e948f0d72c8ca6e848
Leonardo <leonardo.lopes555@gmail.com>
2020-06-01 19:01:19 -0300

    adicionando outras propiedades dentro do pokémon

--- src/pages/details.js
+++ src/pages/details.js
@@ -22,8 +22,9 @@ export default class Details extends Component {
 
     state = {
         info: [],
-        abilities:[],
         sprites:[],
+        types: [],
+        base: [],
     };
     
 
@@ -31,6 +32,10 @@ export default class Details extends Component {
         Axios.get(this.props.navigation.getParam('Link')).then(res => {
           const sprites = res.data.sprites.front_default;
           const info = res.data.abilities;
+          const types = res.data.types;
+          const base = res.data;
+          this.setState ({base})
+          this.setState ({types})
           this.setState ({info})
           this.setState({ sprites });
           
@@ -44,22 +49,50 @@ export default class Details extends Component {
 
 
             <View style = {styles.Container}>
-
-            <Text style = {styles.Abilities}>Imagem do Pokémon:</Text>  
-
-            <Image style = {{width:250, height:150}} source={{ uri: this.state.sprites.toString() }} />
-
-            <Text style = {styles.Abilities}>Habilidades:</Text>  
-
-            {this.state.info.map(info => {
-              
-              return <View style = {styles.Box}>
-        
-                        <Text style = {styles.Text}>* {info.ability.name}</Text>
-                    </View>; 
-            })}
-        
             
+            <View style = {styles.imgback}>
+                <Text style = {styles.ImgText}>Imagem do Pokémon:</Text>  
+                <Image style = {styles.Image} source={{ uri: this.state.sprites.toString()}} />
+            </View>
+
+            <View style = {styles.Background}>
+                <Text style = {styles.Abilities}>Habilidades:</Text>  
+
+                        {this.state.info.map(info => {
+                    
+                            return <View style = {styles.Box}>
+                
+                            <Text style = {styles.Text}>* {info.ability.name}</Text>
+
+                            </View>; 
+                        })}
+
+                <Text style = {styles.Type}>Tipo:</Text>
+
+                        {this.state.types.map(types => {
+
+                            return <View style = {styles.Box}>
+
+                            <Text style = {styles.Text}>* {types.type.name}</Text>
+
+                            </View>; 
+
+                        })}
+
+
+                <Text style = {styles.Base}>Base de experiência:</Text>
+
+                <Text style = {styles.Text}>{this.state.base.base_experience}</Text>
+
+                <Text style = {styles.Weight}>Largura:</Text>
+
+                <Text style = {styles.Text}>{this.state.base.weight}</Text>
+
+                <Text style = {styles.height}>Altura:</Text>
+
+                <Text style = {styles.Text}>{this.state.base.height}</Text>
+            </View>
+
             </View>
 
 
@@ -69,24 +102,90 @@ export default class Details extends Component {
 
 
 const styles = StyleSheet.create ({
-    
-    Container:{
-        paddingLeft:22,
-        borderBottomColor: "#3a86ff",
-        paddingBottom:10,
-        marginTop:10,
-        marginBottom:5,
+
+    Background:{
+        backgroundColor:"#a61717",
     },
 
-    Abilities: {
-        fontSize:29,
+    Image:{
+        width:"50%",
+        height:150,
+        textAlign:"center",
+        alignItems:"center",
+        justifyContent:"center",
+        marginBottom:5,
+        padding:10,
+        marginLeft:"auto",
+        marginRight:"auto",
+    },
+
+    ImgText: {
+        fontSize:25,
         color:"#000",
         fontWeight:"bold",
+        paddingLeft:10,
+        textAlign:"center",
+    },
+
+    Box: {
+        borderBottomColor:"#ccc",
+    },
+
+
+    Abilities: {
+        fontSize:25,
+        color:"#ffca08",
+        fontWeight:"bold",
+        paddingLeft:10,
+        textAlign:"center",
+    },
+
+    Type: {
+        fontSize:25,
+        color:"#ffca08",
+        fontWeight:"bold",
+        paddingLeft:10,
+        textAlign:"center",
+
+    },
+
+    Base:{
+        fontSize:25,
+        color:"#ffca08",
+        fontWeight:"bold",
+        paddingLeft:10,
+        textAlign:"center",
+
+    },
+
+    Weight:{
+        fontSize:25,
+        color:"#ffca08",
+        fontWeight:"bold",
+        paddingLeft:10,
+        textAlign:"center",
+
+    },
+
+    height: {
+        fontSize:25,
+        color:"#ffca08",
+        fontWeight:"bold",
+        paddingLeft:10,
+        textAlign:"center",
+
     },
 
     Text: {
         fontSize:22,
-        color:"#000",
+        color:"#fff",
+        paddingLeft:10,
+        textAlign:"center",
+        borderBottomColor:"#ccc",
+        marginBottom:5,
+
     },
+
+
 
 });
